bbc9ec8eaed30c50b1af49e6a79a9543090a8a0f
Jakas <84695818+phogat20@users.noreply.github.com>
2023-02-11 22:52:07 +0530

    Add files via upload

---
+++ Analog Clock/index.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
